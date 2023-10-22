fb675d3d17403cc330f92c9abcc1b2618b0a5863
Em leons <64161260+emleonstz@users.noreply.github.com>
2023-10-22 10:56:34 +0300

    Update Encryption.php

--- app/Config/Encryption.php
+++ app/Config/Encryption.php
@@ -21,7 +21,7 @@ class Encryption extends BaseConfig
      * You need to ensure it is long enough for the cipher and mode you plan to use.
      * See the user guide for more info.
      */
-    public string $key = 'base64:6R2flH31OROQRFH5mmOHZJqVFdz/cLdxGNifWjWfjN4=';
+    public string $key = 'your_key_hapa';
 
     /**
      * --------------------------------------------------------------------------
